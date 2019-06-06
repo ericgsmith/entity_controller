9f107a1ef7a160f40ab93582b67221bfcc8a5294
Eric Smith <ericgsmith80@gmail.com>
2019-06-06 15:47:18 +1200

    Updated doc

--- src/Plugin/EntityFacadeInterface.php
+++ src/Plugin/EntityFacadeInterface.php
@@ -6,6 +6,9 @@ use Drupal\Component\Plugin\PluginInspectionInterface;
 
 /**
  * Defines an interface for entity facades.
+ *
+ * There are no explicit methods on the face interface. Instead facades themselves
+ * broker the interactions to the entity that is covered by each plugin.
  */
 interface EntityFacadeInterface extends PluginInspectionInterface {
 
